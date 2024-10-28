84e654549c0a731767df26477269243d3fad3fe7
Carlos-Cardenas-Bravo <cardenasbravo@gmail.com>
2024-10-28 09:03:11 -0300

    se corrige la carga de image_profile en S3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
     @job_applications = @user.job_applications
     render 'profile'
   end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_profile_path, notice: 'Perfil actualizado correctamente.'
+    else
+      render 'profile'
+    end
+  end
+
+  private
+
+  # Permitir los parámetros necesarios, incluido image_profile
+  def user_params
+    params.require(:user).permit(:name, :email, :experience_years, :education_level, :previous_positions, :password, :image_profile)
+  end
 end
